0114d0d5a78a889e7890d98d05c43c693a0cb6f0
Shitanshu Kumar <shitanshu.kumar@impactanalytics.co>
2026-06-09 09:19:47 +0530

    fix: update comments for clarity in App component routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,4 @@
+// This is the main application component that sets up routing for the entire app. It includes a Navbar and defines routes for different pages such as Home, Leaderboard, Sign In, Sign Up, Create Posts, and Feed. The Create Posts and Feed routes are protected and require authentication to access. If a user tries to access a route that doesn't exist, they will be directed to a Not Found page.
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import CreatePosts from "./pages/CreatePosts";
@@ -14,26 +15,26 @@ const App = () => {
     <>
       <Navbar />
       <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/leaderboard" element={<Leaderboard />} />
-      <Route path="/sign-in/*" element={<SignInPage />} />
-      <Route path="/sign-up/*" element={<SignUpPage />} />
-      <Route
-        path="/create_a_post"
-        element={
-          <ProtectedRoute>
-            <CreatePosts />
-          </ProtectedRoute>
-        }
-      />
-      <Route
-        path="/get_all_posts"
-        element={
-          <ProtectedRoute>
-            <Feed />
-          </ProtectedRoute>
-        }
-      />
+        <Route path="/" element={<Home />} />
+        <Route path="/leaderboard" element={<Leaderboard />} />
+        <Route path="/sign-in/*" element={<SignInPage />} />
+        <Route path="/sign-up/*" element={<SignUpPage />} />
+        <Route
+          path="/create_a_post"
+          element={
+            <ProtectedRoute>
+              <CreatePosts />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/get_all_posts"
+          element={
+            <ProtectedRoute>
+              <Feed />
+            </ProtectedRoute>
+          }
+        />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </>
